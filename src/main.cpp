b31d0288b46eaccb24e8d1c3777b6b77d19f0c32
ilotterytea <ilotterytea@proton.me>
2023-09-17 23:27:58 +0600

    feat: twitch client added in main lesgooo

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "client/twitch.h"
 #include "env_options.h"
 #include <iostream>
 #include <optional>
@@ -10,6 +11,18 @@ int main(int argc, char *argv[]) {
   if (!options.has_value()) {
     std::cerr << "No options file!" << std::endl;
     return 1;
+  }
+
+  TwitchIRCClient ircClient(options->username, options->password);
+
+  ircClient.on<IRCMessageType::PrivmsgMessage>([&ircClient](std::string msg) {
+    ircClient.say("ilotterytea", "juuuuuuuuuuuuuuuuuuuustrl ");
+  });
+
+  ircClient.connect();
+  ircClient.join("ilotterytea");
+
+  while (true) {
   }
 
   return 0;
