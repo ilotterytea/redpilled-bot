1c6262c6042c345f960bda332311c687515bdf7f
ilotterytea <ilotterytea@proton.me>
2023-09-18 20:48:59 +0600

    feat: i coded this shit

--- src/client/twitch_message.cpp
+++ src/client/twitch_message.cpp
@@ -1,9 +1,27 @@
 #include "twitch_message.h"
 #include "../utils/string.h"
 #include <optional>
+#include <string>
 #include <vector>
 
-// TODO: code this shit
 std::optional<IRCMessageType> define_message_type(const std::string &msg) {
+  std::vector<std::string> vec = split(msg, " ");
+  std::string stringType;
+  IRCMessageType type;
+
+  if (vec[0][0] == '@') {
+    stringType = vec[2];
+  } else if (vec[0][0] == ':') {
+    stringType = vec[1];
+  } else {
+    return std::nullopt;
+  }
+
+  if (stringType == "PRIVMSG") {
+    return IRCMessageType::PrivmsgMessage;
+  } else {
+    return std::nullopt;
+  }
+
   return std::nullopt;
 }
